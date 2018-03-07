af22f71cf1fb54b6f344700a01a04e3bc9ac001c
Marine Lira <mlira@ensc.fr>
2018-03-07 17:08:40 +0100

    creation des fichiers, classes architecture ok
    Signed-off-by: Marine Lira <mlira@ensc.fr>

---
+++ ProjetIASalotti/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetIASalotti
+{
+    class Class1 : GenericNode
+    {
+        public override bool IsEqual(GenericNode N2)
+        {return true ; }
+        public override double GetArcCost(GenericNode N2)
+        { return 12; }
+        public override bool EndState()
+        {return true; }
+        //public override List<GenericNode> GetListSucc()
+        //{ ; }
+        public override double CalculeHCost()
+        {return 12; }
+    }
+}
